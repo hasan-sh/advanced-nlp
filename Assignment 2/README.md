b0bef271e491f3cc1388fd3f0c3d6f2f597a716a
GabHoo <gabriel.hoogerwerf@gmail.com>
2023-02-28 15:59:48 +0100

    baseline and readme

---
+++ Assignment 2/README.md
@@ -0,0 +1,3 @@
+To run the the script please locate your training and test orginal dataset and give it to the main file as follows:
+
+$python3 main.py [data_folder_path]
--- Assignment 2/baseline.py
+++ Assignment 2/baseline.py
@@ -1,20 +1,22 @@
-
-import pandas as pd
-import numpy as np
-import sys
-from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import *
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.feature_extraction import FeatureHasher
-
+from utils import *
 from warnings import simplefilter
-from sklearn.exceptions import ConvergenceWarning
+import sys
+simplefilter("ignore")
+N = 800000 # downsample coeffitient
 
-from utils_gabhoo import *
-simplefilter("ignore", category=ConvergenceWarning)
 
+def read_dfs():
+    train_file = '../data/train_ner.tsv'
+    test_file = '../data/test_ner.tsv'
+    train_df = pd.read_csv(train_file, delimiter='\t')
+    test_df = pd.read_csv(test_file, delimiter='\t')
+    
+    train_df = train_df[train_df['target'].notna()]
+    test_df = test_df[test_df['target'].notna()]
+    
+    train_df = train_df[train_df["distance_head"]!="_"].reset_index(drop=True)
+    test_df = test_df[test_df["distance_head"]!="_"].reset_index(drop=True)
+    return train_df, test_df
 
 def main(argv, arc):
 
@@ -27,33 +29,15 @@ def main(argv, arc):
         data_path=argv[1]
 
   
-
-              
-    """train_ner = data_path+'/train_ner.tsv'
-    test_ner = data_path+'/test_ner.tsv'
-    train_ner_df = pd.read_csv(train_ner, delimiter='\t')
-    test_ner_df = pd.read_csv(test_ner, delimiter='\t')"""
-
  
-    train_file = data_path+'/en_ewt-up-train.conllu'
-    test_file = data_path+'/en_ewt-up-test.conllu'
-    train = read_data(train_file,save_to_csv=True)
-    test = read_data(test_file, save_to_csv=True)
-    train.reset_index(inplace=True)
-    test.reset_index(inplace=True)
-
+    print('Reading Data and Engineering features ...')
+    # train_df, test_df = read_conll(data_path)
+    train_df, test_df = read_dfs()
     
 
-    train_df=train[['token','target']] #BECAUSE ITS BASELINE
-    test_df=test[['token','target']]
-
-
-    train_df=downsample(train_df,820000)
-    train_df.reset_index(inplace=True) #necessary after the down sample
-
-
-
     X_train,X_test = vectorize_hash_token(train_df.token,test_df.token)
+    helper_train, helper_test = get_helper1(X_train, X_test, train_df['target'], test_df['target'])
+
 
     #here you do all the shit with the other features
 
@@ -64,24 +48,18 @@ def main(argv, arc):
     X_train['helper1']=helper_train
     X_test['helper1']=helper_test
 
+    y_pred = logistic_reg(X_train,X_test,y_train,y_test)
+    print('Done')
+    
+    evaluate(y_test, y_pred)
+    
+    # Proper per class evaluation
+    labels = list(set(y_train))
 
+    report = classification_report(y_test, y_pred,
+                                   target_names=[mapping[i] for i in labels])
+    print(report)
 
-    y_train,y_test=make_cat_label(train_df['target'], test_df['target'])
-    #instantiate the model
-    log_regression = LogisticRegression(penalty='l2')
-
-    #fit the model using the training data
-    log_regression.fit(X_train,y_train)
-
-    #use model to make predictions on test data
-    y_pred = log_regression.predict(X_test)
-
-    f1 = f1_score(y_test,y_pred, average='weighted')
-    prec = precision_score(y_test,y_pred, average='weighted')
-    rec = recall_score(y_test,y_pred,average='weighted')
-    print(f"{f1=}")
-    print(f"{prec=}")
-    print(f'{rec=}')
 
 
 
